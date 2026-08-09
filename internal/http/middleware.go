6af852e1f0b36ddbc2c464902c7ac890a7b71ba6
Eduardo Costa <eduardocosta942z@gmail.com>
2026-08-08 22:40:29 -0300

    fix: CORS

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -4,7 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Vary", "Origin")
+		}
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
